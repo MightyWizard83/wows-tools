c28992f28628fc5470e47fd1662283efb53dd72a
Giovanni Acciai <MightyWizard83@gmail.com>
2018-08-26 22:02:47 +0200

    clean code

--- config/app.php
+++ config/app.php
@@ -161,11 +161,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         
-        /*
-         * Custom Service Providers
-         */
-        
-
     ],
 
     /*
